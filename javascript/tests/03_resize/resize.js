692f374b8b42da0011dc504dd62c3db7abbf7195
John Blossom <jblossom.web@gmail.com>
2019-11-11 18:01:03 -0700

    test 3: vanilla resize

--- javascript/tests/03_resize/resize.js
+++ javascript/tests/03_resize/resize.js
@@ -4,7 +4,21 @@ const initResize = () => {
      * and drags the `resize` node, its parent node `panel` should grow or shrink
      * vertically.
      */
+    const panel = document.getElementById('panel');
     const resize = document.getElementById('resize');
+
+    const handleMove = e => {
+        const newHeight = window.innerHeight - e.pageY;
+        panel.style.height = `${newHeight}px`;
+    };
+
+    resize.onmousedown = () => {
+        document.addEventListener('mousemove', handleMove);
+        document.onmouseup = () => {
+            document.removeEventListener('mousemove', handleMove);
+            document.onmouseup = null;
+        };
+    };
 };
 
 window.initResize = initResize;
